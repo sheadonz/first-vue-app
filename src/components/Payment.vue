62202264b9dc780666cac38bec4bb2c27880df68
sheadonz <njxb.441@gmail.com>
2024-04-23 21:44:59 +0900

    initial

---
+++ src/components/Payment.vue
@@ -0,0 +1,34 @@
+<!-- <scriptsetup lang="ts">
+
+</scriptsetup> -->
+
+<template>
+    <div class="container">
+        <h1>最近の支出</h1>
+        <div class="payment">
+            <label>カレーライス</label>
+            <label>400円</label>
+        </div>
+        
+    </div>
+</template>
+
+
+<style scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.payment{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 80px;
+    width: 400px;
+    background-color:darkturquoise
+}
+label {
+    font-size: 20px;
+}
+</style>
